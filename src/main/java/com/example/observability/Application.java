e1a9008f1a203e1d0e892b0de6d4637cbb8a886f
Igor Cardoso Vieira <igorcardoso907@gmail.com>
2023-11-25 22:39:24 -0300

    Tracing Monitoring: OpenTelemetry + Jaeger/Zipkin

--- src/main/java/com/example/observability/Application.java
+++ src/main/java/com/example/observability/Application.java
@@ -2,6 +2,12 @@ package com.example.observability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +16,24 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@RestController
+	class HelloController {
+		private final RestTemplate restTemplate;
+
+		public HelloController(RestTemplate restTemplate) {
+			this.restTemplate = restTemplate;
+		}
+
+		@GetMapping("/hello")
+		public String hello() {
+			ResponseEntity<String> responseEntity =
+					restTemplate.postForEntity("https://httpbin.org/post", "Hello, Cloud!", String.class);
+			return responseEntity.getBody();
+		}
+	}
 }
